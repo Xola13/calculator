7449cd73d6e69b581d1ad46064f0e23ec7ef4af0
Xola13 <91262470+Xola13@users.noreply.github.com>
2021-11-23 16:33:50 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,14 @@
+let sum="";
+function addTosum(operator) {
+    sum=sum+operator;
+    document.getElementById('output').innerText = sum;
+}
+
+function evaluatesum () {
+    document.getElementById('output').innerText = eval(sum);
+}
+function clearsum() {
+    sum="";
+    document.getElementById('output').innerText = sum;
+}
+
